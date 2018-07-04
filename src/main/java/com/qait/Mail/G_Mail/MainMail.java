4c6d8667327bae14a0d022b5568597b8ee532b4c
Himanshu Sharma <40353487+HimanshuQAIT@users.noreply.github.com>
2018-07-04 11:58:02 +0530

    Update MainMail.java

--- src/main/java/com/qait/Mail/G_Mail/MainMail.java
+++ src/main/java/com/qait/Mail/G_Mail/MainMail.java
@@ -1,9 +1,9 @@
 package com.qait.Mail.G_Mail;
 
-import java.awt.Window;
+//import java.awt.Window;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -22,19 +22,45 @@ public class MainMail {
 		wd.get("https://www.google.com/gmail/about/#");
 	}
 
-	public void checkMenuItemForWork() {
+	public void checkMenuItemForWork() throws InterruptedException {
 		wd.findElement(By.xpath("/html/body/nav/div/a[1]")).click();
 		wd.navigate().back();
+		Thread.sleep(2000);
 	}
-
-	public void checkMenuItemCreateAnAccount() {
-		wd.findElement(By.xpath("/html/body/nav/div/a[3]")).click();
-		wd.navigate().back();
-	}
-
-	public void checkMenuItemSignIn() {
+	
+	public void checkMenuItemSignIn() throws InterruptedException {
 		wd.findElement(By.xpath("/html/body/nav/div/a[2]")).click();
 		wd.navigate().back();
+		Thread.sleep(2000);
 	}
 
+	public void checkMenuItemCreateAnAccount() throws InterruptedException {
+		wd.findElement(By.xpath("/html/body/nav/div/a[3]")).click();
+		wd.navigate().back();
+		Thread.sleep(2000);
+	}
+
+	
+	
+	public void enterFirstName() {
+		wd.findElement(By.xpath("//*[@id='firstName']")).sendKeys("Rakesh");
+
+	}
+
+	public void enterLastName() {
+		wd.findElement(By.id("lastName")).sendKeys("Kumar");
+
+	}
+
+	public void enterUsername() {
+		wd.findElement(By.xpath("//*[@id=\"username\"]")).sendKeys("rakeshkumar00");
+	}
+
+	public void enterPassword() {
+		wd.findElement(By.xpath("//*[@id=\"passwd\"]/div[1]/div/div[1]/input")).sendKeys("12345678");
+	}
+
+	public void enterConfirmPassword() {
+		wd.findElement(By.xpath("//*[@id=\"confirm-passwd\"]/div[1]/div/div[1]/input")).sendKeys("12345678");
+	}
 }
