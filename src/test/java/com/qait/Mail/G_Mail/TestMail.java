80120c3404449209aaf0ef5cdcfb6f519ef56ff9
Himanshu Sharma <40353487+HimanshuQAIT@users.noreply.github.com>
2018-07-04 11:58:51 +0530

    Update TestMail.java

--- src/test/java/com/qait/Mail/G_Mail/TestMail.java
+++ src/test/java/com/qait/Mail/G_Mail/TestMail.java
@@ -2,6 +2,8 @@ package com.qait.Mail.G_Mail;
 
 import org.testng.annotations.Test;
 
+import junit.framework.Assert;
+
 import org.testng.AssertJUnit;
 import org.testng.annotations.BeforeTest;
 
@@ -23,13 +25,40 @@ public class TestMail {
 		}
 
 		@Test(priority = 1)
-		public void CheckMenuItemForWork() {
+		public void CheckMenuItemGmail() throws InterruptedException {
 			tat.checkMenuItemForWork();
 			tat.checkMenuItemSignIn();
 			tat.checkMenuItemCreateAnAccount();
+			//
 			AssertJUnit.assertTrue(tat.wd.getCurrentUrl().contains("gmail"));
+			Thread.sleep(2000);
 		}
-
+		
+		@Test(priority = 2)
+		public void EnterFirstName() {
+			tat.enterFirstName();
+			
+			Assert.assertTrue(tat.wd.getCurrentUrl().contains("gmail"));
+		}
+        
+		@Test(priority = 3)
+		public void EnterLastName() {
+			tat.enterLastName();
+			Assert.assertTrue(tat.wd.getCurrentUrl().contains("gmail"));
+		
+		}
+		
+		@Test(priority = 4)
+		public void EnterUsername() {
+			tat.enterUsername();
+			Assert.assertTrue(tat.wd.getCurrentUrl().contains("gmail"));
+		}
+		
+		@Test(priority = 5)
+		public void EnterPassword() {
+			tat.enterPassword();
+			Assert.assertTrue(tat.wd.getCurrentUrl().contains("gmail"));
+		}
 		
   }
 
